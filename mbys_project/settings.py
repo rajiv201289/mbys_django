95b90b258abf79e0508b5e4485dee725322031f1
rajiv <rajiv.rjvcode@gmail.com>
2024-12-14 15:01:38 +0530

    dep1

--- mbys_project/settings.py
+++ mbys_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY =os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','mbys.eu-north-1.elasticbeanstalk.com']
 
