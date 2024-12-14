8425f65e008b63addf07a5a400f22429118de2ac
rajiv <rajiv.rjvcode@gmail.com>
2024-12-14 14:21:12 +0530

    changed secret key and stored it in the env file

--- mbys_project/settings.py
+++ mbys_project/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from dotenv import load_dotenv
+import os 
 from pathlib import Path
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-((#)y!-@p5xj(^z7z5g=4nnci_383cmz%=i%r2n*x0b1ltsf_!'
+SECRET_KEY =os.environ.get('SECRET_KEY')
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','mbys.eu-north-1.elasticbeanstalk.com']
 
